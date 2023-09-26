c99da50bbefbd95d22a7703e49db34db7c39c5a4
luyang93 <39150988+luyang93@users.noreply.github.com>
2023-09-26 12:40:04 +0800

    dev

--- conf.js
+++ conf.js
@@ -85,11 +85,8 @@ jsproxy_config({
    * URL 自定义处理（设计中）
    */
   url_handler: {
-    'https://www.baidu.com/img/baidu_resultlogo@2.png': {
-      replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
-    },
     'https://www.pornhub.com/': {
-      redir: 'https://blog.51sec.org/'
+      redir: 'https://www.360.com/'
     },
     'http://haha.com/': {
       content: 'Hello World'
